d6beb94fcf2c34feb57ccb4ea1cf89f50b6c101c
vm <vm@healthcare-data.iiit.ac.in>
2023-05-01 16:42:38 +0000

    init local containers

--- local.sh
+++ local.sh
@@ -1,4 +1,4 @@
-sudo docker stop dev-healthcare-reactjs dev-healthcare-nodejs dev-healthcare-flask
-sudo docker rm dev-healthcare-reactjs dev-healthcare-nodejs dev-healthcare-flask
-sudo docker compose -f docker-compose-dev.yml --env-file dev.env build
-sudo docker compose -f docker-compose-dev.yml --env-file dev.env up -d
+sudo docker stop local-healthcare-reactjs local-healthcare-nodejs local-healthcare-flask
+sudo docker rm local-healthcare-reactjs local-healthcare-nodejs local-healthcare-flask
+sudo docker compose -f docker-compose-local.yml --env-file local.env build
+sudo docker compose -f docker-compose-local.yml --env-file local.env up -d
